af7a776570ba83aef6df3d0be3b61431b35636f2
Leonel R <leorhcp2030@gmail.com>
2018-07-13 00:47:49 -0300

    Update tda.py

--- tda.py
+++ tda.py
@@ -1,1 +1,26 @@
+class Grafo:
+  
+  def __init__(self):
+    self.vertices = {}
+    self.cant_v = 0
+    self.cant_e = 0
+    
+  def v(self):
+    return self.cant
+
+  def agregar_v(self, nombre, dato):
+    
+    if not nombre in self.vertices:
+      v = Vertice(nombre, dato)
+      self.vertice[nombre] = v
+      self.cant_v += 1
+  
+class Vertice:
+  
+  def __init__(self, nombre, dato):
+    self.nombre = nombre
+    self.dato = dato
+  
+  def __str__(self):
+    return str(self.nombre, self.dato)
 
